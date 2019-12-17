85c9d4aa77b535927132f6a0b9758c047c44939b
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-12-17 15:46:06 -0500

    Removed unused usings

--- src/gemstone.pqdif/Physical/ScalarElement.cs
+++ src/gemstone.pqdif/Physical/ScalarElement.cs
@@ -24,8 +24,6 @@
 //******************************************************************************************************
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 using System.Text;
 
